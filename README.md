e6c87d74be63359f83f31cbceffda51c181b70f0
Oscar Palmér <opalmer@me.com>
2017-11-30 17:58:32 +0100

    0.4.0
    minified skrolla and skrolla.babel
    
    added:
      uglify
    
    readme:
      updated to-do

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A tiny plugin for scrolling to specific places on a page. :cyclone:
 
 ## Getting started
 
-_Skrolla_ is available via [npm](//npmjs.com/package/skrolla) and [yarn](//yarnpkg.com/en/package/skrolla), but you can just as easily download [the script](src/skrolla.js) and then include it in your document. After that, all that's needed is to add the `[data-skrolla]`-attribute to clickable elements with its value pointing to an existing ID in your HTML. :sunglasses:
+_Skrolla_ is available via [npm](//npmjs.com/package/skrolla) and [yarn](//yarnpkg.com/en/package/skrolla), but you can just as easily download [the script](dist/skrolla.js) and then include it in your document. After that, all that's needed is to add the `[data-skrolla]`-attribute to clickable elements with its value pointing to an existing ID in your HTML. :sunglasses:
 
 ## Older browsers
 
@@ -14,9 +14,8 @@ _Skrolla_ uses a few things introduced in later version of [ECMAScript](//en.wik
 
 ## To-do
 
-1. Minification
-2. Comments
-3. Docs & demo
+1. Comments
+2. Docs & demo
 
 ## License
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const
-gulp   = require("gulp"),
-babel  = require("gulp-babel"),
-eslint = require("gulp-eslint"),
-rename = require("gulp-rename"),
-pump   = require("pump");
+gulp     = require("gulp"),
+babel    = require("gulp-babel"),
+composer = require("gulp-uglify/composer"),
+eslint   = require("gulp-eslint"),
+rename   = require("gulp-rename"),
+pump     = require("pump"),
+uglify   = require("uglify-es"),
+
+//  Uglify + ES6+
+minify   = composer(uglify, console);
 
 //  Task for finding errors and problems in Skrolla
 gulp.task("eslint", () => {
@@ -15,12 +20,23 @@ gulp.task("eslint", () => {
   ]);
 });
 
-//  Task for running Skrolla through Babel
+//  Task for running Skrolla through Babel,
+//  and then minifying it
 gulp.task("babel", () => {
   pump([
     gulp.src("src/skrolla.js"),
     babel({ presets: ["env"] }),
+    minify(),
     rename("skrolla.babel.js"),
+    gulp.dest("dist")
+  ]);
+});
+
+//  Task for minifying Skrolla
+gulp.task("minify", () => {
+  pump([
+    gulp.src("src/skrolla.js"),
+    minify(),
     gulp.dest("dist")
   ]);
 });
