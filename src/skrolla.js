2687e3ae81c08efd00123900f914970827ea4a92
Oscar Palmér <opalmer@me.com>
2019-07-16 18:27:20 +0200

    0.8.0
    + refactoring
    + minification with terser

--- src/skrolla.js
+++ src/skrolla.js
@@ -1,238 +1,266 @@
-((scope) => {
-  //  Document for 'scope'
-  const doc = scope.document;
+/*!
+ * Skrolla, v0.8.0 - for scrolling to specific places on a page!
+ * https://github.com/oscarpalmer/skrolla
+ * (c) Oscar Palmér, 2019, MIT @license
+ */
+// eslint-disable-next-line no-unused-vars
+const skrolla = (function skrolla() {
+  // Reference to document window
+  const win = window;
+  // Document object for document window
+  const doc = win.document;
 
-  //  References for smarter math functions
-  const ceiling = Math.ceil;
-  const powerOf = Math.pow;
-
-  //  Reference for smarter array and object functions
-  const arrayPrototype = Array.prototype;
-  const objectPrototype = Object.prototype;
-
-  //  Options for Skrolla
-  const options = {
-    callback: null,
-    offset: 0,
+  /**
+   * State variables and options for Skrolla.
+   */
+  const Skrolla = {
+    // Does a callback exist?
+    hasCallback: false,
+    // Is Skrolla active?
+    isActive: false,
+    options: {
+      // No callback by default
+      callback: null,
+      // No offset for scrolling by default
+      offset: 0,
+    },
   };
 
   /**
-   *  Add listeners to the appropriate elements.
+   * Collection of animation methods.
    */
-  const addListeners = () => {
-    //  Elements that are Skrolla-friendly
-    const elements = doc.querySelectorAll('[data-skrolla]');
+  const Animation = {
+    /**
+     * 'in-out-cubic'-easing function for nice animations.
+     * Thanks, Benjamin; github.com/bendc/animateplus
+     * @param {Float} progress Value for calculations
+     * @return {Float} Calculated value
+     */
+    easing(progress) {
+      const timesTwo = progress * 2;
 
-    //  Add an event listener for each element
-    Array.prototype.forEach.call(elements, (element) => {
-      element.addEventListener('click', onClick);
-    });
-  };
+      if (timesTwo < 1) {
+        return 0.5 * (timesTwo ** 3);
+      }
 
-  /**
-   *  easeInOutQuad-easing function for nice animations.
-   *
-   *  Thanks, Benjamin; github.com/bendc/animateplus
-   *
-   *  @param  {Float} time - Value for calculations
-   *  @return {Float}        Calculated value
-   */
-  const easing = (time) => {
-    if ((time *= 2) < 1) {
-      return 0.5 * powerOf(time, 3);
-    }
+      const minusTwo = timesTwo - 2;
 
-    return 0.5 * ((time -= 2) * powerOf(time, 2) + 2);
-  };
-
-  /**
-   *  Get the duration for a single animation.
-   *
-   *  Distance can be negative, but duration cannot,
-   *  so let's make it positive. A positive distance might
-   *  still be float-y, so let's get its nearest greater integer.
-   *  After all that, we'll return it.
-   *
-   *  @param  {Number} distance - Distance to scroll
-   *  @return {Number}            Duration of scroll
-   */
-  const getDuration = distance => ceiling(distance < 1 ? -distance : distance);
-
-  /**
-   *  Get an element based on an ID,
-   *  or the document body if that's not possible.
-   *
-   *  @param  {String}  id - ID for element
-   *  @return {Element}      Found element
-   */
-  const getElement = id => doc.getElementById(id) || doc.body;
-
-  /**
-   *  Get the offset value of an element.
-   *
-   *  @param  {Element} element - Element to inspect
-   *  @return {Number}            Offset value
-   */
-  const getOffset = element => element.getBoundingClientRect().top - options.offset;
-
-  /**
-   *  Get the target element for a specific scroll.
-   *
-   *  @param  {Element|String} target - ID for, or an actual element
-   *  @return {Element}                 Found element
-   */
-  const getTarget = (target) => {
-    //  The target is already an element,
-    //  so let's just return it
-    if (target.nodeType) {
-      return target;
-    }
-
-    //  Call 'getElement' if it's a string and return its value,
-    //  or return the document body if the target is invalid
-    return typeof target === 'string' ? getElement(target) : doc.body;
-  };
-
-  /**
-   *  The handler for click events added to elements in 'addListeners'.
-   *
-   *  @param {Event} event - Event details for a unique element
-   */
-  const onClick = (event) => {
-    //  Prevent unneseccary events, like those for anchors
-    event.preventDefault();
-
-    //  Get the target of the event
-    const { target } = event;
-
-    //  Get the value of the 'data-skrolla'-attribute on the target
-    const targetId = target.getAttribute('data-skrolla');
-
-    //  Call 'scrollTo' with both the targeted value and the event target
-    skrollTo(targetId, target);
-  };
-
-  /**
-   *  Override the default options with ones supplied by the user.
-   *
-   *  @param {Object} object - Object of options
-   */
-  const setOptions = (object) => {
-    //  Bad options, so let's not do anything with it
-    if (objectPrototype.toString.call(object) !== '[object Object]') {
-      return;
-    }
-
-    //  For each option, override its counterpart in the default options
-    arrayPrototype.forEach.call(Object.keys(object), (key) => {
-      options[key] = object[key];
-    });
-
-    //  If the user added a 'callback', we'll tell Skrolla about it
-    if (typeof options.callback === 'function') {
-      skrollaCallback = true;
-    }
-  };
-
-  /**
-   *  The actual scrolling function for Skrolla.
-   *
-   *  Called by both event handlers and 'skrolla.to';
-   *
-   *  @param {Element|String} target - ID for, or element to scroll to
-   *  @param {Element|Window} origin - Origin element or 'window'
-   */
-  const skrollTo = (target, origin) => {
-    //  Offset for the page
-    const page = scope.pageYOffset;
-
-    //  Target element for the scroll
-    const element = getTarget(target);
-
-    //  Offset for the target element
-    const offset = getOffset(element);
-
-    //  Duration for the scroll
-    const duration = getDuration(offset);
+      return 0.5 * (minusTwo * (minusTwo ** 2) + 2);
+    },
 
     /**
-     *  Looped function for the scroll.
-     *
-     *  @param {Float} time - Current time
+     * Get the absolute and highest value for duration for a single animation.
+     * @param {Number} distance Distance to scroll
+     * @return {Number} Duration of scroll
      */
-    const loop = (time) => {
-      //  First loop? Set the starting time
-      if (start === null) {
-        start = time;
-      }
-
-      //  Elapsed time for the animation
-      const elapsed = time - start;
-
-      //  Distance to cover during this incremental scroll
-      const distance = offset * easing(elapsed / duration);
-
-      //  Scroll to a specific position
-      scope.scrollTo(0, page + distance);
-
-      if (elapsed < duration) {
-        //  Animation and scrolling hasn't finished,
-        //  so let's call rFA to work its magic again
-        return scope.requestAnimationFrame(loop);
-      }
-
-      if (skrollaCallback) {
-      //  If the user added a callback, we'll call
-      //  it now that the scrolling has finished
-        return options.callback.call(element, element, origin);
-      }
-
-      return null;
-    };
-
-    //  Start value for the animation,
-    //  to be overriden with the start of the first loop
-    let start = null;
-
-    //  Call rFA with our 'loop'-function
-    scope.requestAnimationFrame(loop);
+    getDuration(distance) {
+      return Math.ceil(Math.abs(distance));
+    },
   };
 
-  //  Skrolla is not active by default.
-  let skrollaActive = false;
+  /**
+   * Collection of DOM-methods.
+   */
+  const DOM = {
+    /**
+     * Get an element based on an ID,
+     * or the document body if that's not possible.
+     * @param {String} id ID for element
+     * @return {Element} Found element
+     */
+    getElement(id) {
+      return doc.getElementById(id) || doc.body;
+    },
 
-  //  Skrolla does not have a callback by default.
-  let skrollaCallback = false;
+    /**
+     * Get the offset value of an element.
+     * @param {Element} element Element to inspect
+     * @return {Number} Offset value
+     */
+    getOffset(element) {
+      return element.getBoundingClientRect().top - Skrolla.options.offset;
+    },
+
+    /**
+     * Get the target element for a specific scroll.
+     * @param {Element|String} target ID for, or an actual element
+     * @return {Element} Found element
+     */
+    getTarget(target) {
+      // The target is already an element, so let's just return it
+      if (target.nodeType) {
+        return target;
+      }
+
+      // Call 'getElement' if it's a string and return its value,
+      // or return the document body if the target is invalid
+      return typeof target === 'string' ? DOM.getElement(target) : doc.body;
+    },
+  };
 
   /**
-   *  Global method to activate Skrolla.
-   *
-   *  @param {Object} userOptions - Options supplied by the user
+   * Collection of utility methods.
    */
-  scope.skrolla = (userOptions) => {
-    //  If Skrolla is already active,
-    //  it should not be possible to active it again
-    if (skrollaActive) {
+  const Utils = {
+    /**
+     * Override the default options with ones supplied by the user.
+     * @param {Object} options Object of options
+     */
+    setOptions(options) {
+      // The object is bad, or not an actual object, so let's skip it
+      if (options == null
+          || ({}).toString.call(options) !== '[object Object]') {
+        return;
+      }
+
+      // For all items in the object…
+      Object.keys(options).forEach((key) => {
+        // … overwrite matching values in Knuff's options
+        Skrolla.options[key] = options[key];
+      });
+
+      // If a callback was added…
+      if (options.callback != null
+          && typeof options.callback === 'function') {
+        // … set its state variable to true
+        Skrolla.hasCallback = true;
+      }
+    },
+
+    /**
+     * The actual scrolling method for Skrolla,
+     * called by both event handlers and 'skrolla.to';
+     * @param {Element|String} target ID for, or element to scroll to
+     * @param {Element|Window} origin Origin element or 'window'
+     */
+    scrollTo(target, origin) {
+      // Offset value for the page
+      const page = win.pageYOffset;
+
+      // Target element for the scroll
+      const element = DOM.getTarget(target);
+
+      // Offset value for the target element
+      const offset = DOM.getOffset(element);
+
+      // Duration for the scroll
+      const duration = Animation.getDuration(offset);
+
+      // Start value for the animation,
+      // to be overriden with the start of the first loop
+      let start = null;
+
+      /**
+       * Method to loop for the scroll.
+       * @param {Float} time Current time
+       */
+      const loop = (time) => {
+        // First loop? Set the starting time
+        if (start == null) {
+          start = time;
+        }
+
+        // Elapsed time for the animation
+        const elapsed = time - start;
+
+        // Distance to cover during this incremental scroll
+        const distance = offset * Animation.easing(elapsed / duration);
+
+        // Scroll to a specific position
+        win.scrollTo(0, page + distance);
+
+        if (elapsed < duration) {
+          // Animation and scrolling hasn't finished,
+          // so let's call rFA to work its magic again
+          return win.requestAnimationFrame(loop);
+        }
+
+        if (Skrolla.hasCallback) {
+          // If the user added a callback, we'll call
+          // it now that the scrolling has finished
+          return Skrolla.options.callback.call(element, element, origin);
+        }
+
+        return null;
+      };
+
+      // Call 'requestAnimationFrame' with Skrolla's 'loop'-function
+      win.requestAnimationFrame(loop);
+    },
+  };
+
+  /**
+   * Collection of event methods.
+   */
+  const Events = {
+    /**
+     * Method for adding event listeners to relevant elements.
+     */
+    addListeners() {
+      // Get all elements that are Skrolla-friendly
+      const elements = doc.querySelectorAll('[data-skrolla]');
+
+      // Add an event listener for each element
+      Array.prototype.forEach.call(elements, (element) => {
+        element.addEventListener('click', Events.onClick);
+      });
+    },
+
+    /**
+     * The callback for events added to elements in 'addListeners'.
+     * @param {Event} event Event details for a unique element
+     */
+    onClick(event) {
+      // Prevent unnecessary events, like those for anchors
+      event.preventDefault();
+
+      // Get the target of the event
+      const { target } = event;
+
+      // Get the value of the 'data-skrolla'-attribute on the target
+      const targetId = target.getAttribute('data-skrolla');
+
+      // Call 'scrollTo' with both the targeted value and the event target
+      Utils.scrollTo(targetId, target);
+    },
+  };
+
+  /**
+   * Method for initialising Skrolla.
+   * @param {Object} options Object of options
+   */
+  const ret = (options) => {
+    // If Skrolla is already active,
+    // it should not be possible to activate it again
+    if (Skrolla.isActive) {
       return;
     }
 
-    //  Set Skrolla to be active
-    skrollaActive = true;
+    // Set Skrolla to be active
+    Skrolla.isActive = true;
 
-    //  Override the default options
-    setOptions(userOptions);
+    // Override the default options
+    Utils.setOptions(options);
 
-    //  Add event listeners to appropriate elements
-    addListeners();
+    // Add event listeners to appropriate elements
+    Events.addListeners();
   };
 
   /**
-   *  Global method to directly scroll to an element.
-   *
-   *  @param {Element|String} target - ID for, or an existing element
+   * Method to call for adding listeners to dynamic Skrolla-elements,
+   * e.g. when loading elements with AJAX.
    */
-  scope.skrolla.to = (target) => {
-    //  Call 'skrollTo' with the target and 'scope' as parameters
-    skrollTo(target, scope);
+  ret.reload = () => Events.addListeners();
+
+  /**
+   * Global method to directly scroll to an element.
+   * @param {Element|String} target ID for, or an existing element
+   */
+  ret.to = (target) => {
+    // Call 'skrollTo' with the target and window as parameters
+    Utils.scrollTo(target, win);
   };
-})(window);
+
+  // Return and expose public Skrolla methods
+  return ret;
+}());
